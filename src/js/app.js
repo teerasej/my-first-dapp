299153be451d67273f26c3cf3a574e75b1a1404d
Teerasej Jiraphatchandej <teerasej@gmail.com>
2018-11-07 12:08:42 +0700

    ดึงบัญชี coinbase และจำนวน ether มาแสดง

--- src/js/app.js
+++ src/js/app.js
@@ -32,6 +32,29 @@ App = {
 
      displayAccountInfo: function() {
 
+        web3.eth.getCoinbase(function(err, balance){
+              if(err === null){
+                  console.log("No error");
+
+                  App.account = balance;
+                  console.log(balance);
+                  $('#account').text(balance);
+
+                  web3.eth.getBalance(balance, function(err, balance){
+                        if(err === null){
+                              console.log(balance, " Wei");
+                              var ether = web3.fromWei(balance, "ether");
+                              $("#accountBalance").text(ether + " Eth");
+                        } else {
+                              console.log(err);
+                        }
+                  })
+
+              } else {
+                  console.log(err);
+              }
+        })
+
      },
 
      initContract: function() {
