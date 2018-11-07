c38ad3658b8155f651a65d65a7798199eb4425d1
Teerasej Jiraphatchandej <teerasej@gmail.com>
2018-11-07 11:47:12 +0700

    - ใส่ comment เก็บโค้ดเอาไว้ - สร้าง web3 ต่อไปที่ ganache network - สร้าง function เอาไว้ดึงข้อมูล account จาก network

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,37 @@
 App = {
      web3Provider: null,
      contracts: {},
+     account: 0x0,
 
      init: function() {
           
-            var articleRow = $('#articleRow');
-            var articleTemplate = $('#articleTemplate');
+            // var articleRow = $('#articlesRow');
+            // var articleTemplate = $('#articleTemplate');
 
-            articleTemplate.find('.panel-title').text('สินค้าชิ้นที่ 1');
-            articleTemplate.find('.article-description').text('รายละเอียดสินค้าชิ้นที่ 1');
-            articleTemplate.find('.article-price').text('10.24');
-            articleTemplate.find('.article-seller').text('ชื่อผู้ขาย');
+            // articleTemplate.find('.panel-title').text('สินค้าชิ้นที่ 1');
+            // articleTemplate.find('.article-description').text('รายละเอียดสินค้าชิ้นที่ 1');
+            // articleTemplate.find('.article-price').text('10.24');
+            // articleTemplate.find('.article-seller').text('ชื่อผู้ขาย');
 
-            articleRow.append(articleTemplate.html());
+            // articleRow.append(articleTemplate.html());
 
 
           return App.initWeb3();
      },
 
      initWeb3: function() {
-          /*
-           * Replace me...
-           */
+         
+          App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+
+          web3 = new Web3(App.web3Provider);
+
+          App.displayAccountInfo();
 
           return App.initContract();
+     },
+
+     displayAccountInfo: function() {
+
      },
 
      initContract: function() {
