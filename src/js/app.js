004cbceeb4af7ead5ef468c71337262d2c1ce5c7
Teerasej Jiraphatchandej <teerasej@gmail.com>
2018-11-07 11:30:57 +0700

    Show UI of sell contract

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,17 @@ App = {
      contracts: {},
 
      init: function() {
-          /*
-           * Replace me...
-           */
+          
+            var articleRow = $('#articleRow');
+            var articleTemplate = $('#articleTemplate');
+
+            articleTemplate.find('.panel-title').text('สินค้าชิ้นที่ 1');
+            articleTemplate.find('.article-description').text('รายละเอียดสินค้าชิ้นที่ 1');
+            articleTemplate.find('.article-price').text('10.24');
+            articleTemplate.find('.article-seller').text('ชื่อผู้ขาย');
+
+            articleRow.append(articleTemplate.html());
+
 
           return App.initWeb3();
      },
